754f3f680a0d6c6fc0c0f197dcddb296f075ab72
thiag <thiag@DESKTOP-D2BU948>
2020-10-12 23:02:40 -0300

    Só falta botar um tamanho máximo no menu lateral

--- LF-Studio/src/lf/studio/LFStudio.java
+++ LF-Studio/src/lf/studio/LFStudio.java
@@ -1,9 +1,15 @@
 package lf.studio;
 
+import java.awt.Frame;
+import javax.swing.JFrame;
+
 public class LFStudio {
 
     public static void main(String[] args) {
-        
+        Padrao frame = new Padrao();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(frame.getjInternalFrame1().getSize().width, frame.getjInternalFrame1().getSize().height);
+        frame.setVisible(true);
     }
     
 }
